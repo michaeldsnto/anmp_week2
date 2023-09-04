a13ce229dc27da1d4d12ba36fc4c14f85a9fb7bf
michaeldsnto <michaeldesanto80@gmail.com>
2023-09-04 15:30:26 +0700

    init

--- app/src/main/java/com/example/adv160420085week2/MainActivity.kt
+++ app/src/main/java/com/example/adv160420085week2/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.adv160420085week2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = (supportFragmentManager.findFragmentById
+            (R.id.navHostFragment) as NavHostFragment).navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
