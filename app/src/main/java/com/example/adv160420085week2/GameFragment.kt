bbcdc906cbbadf7ef9218bd5cdbc78fed5c733e4
michaeldsnto <michaeldesanto80@gmail.com>
2023-09-06 20:43:09 +0700

    init

--- app/src/main/java/com/example/adv160420085week2/GameFragment.kt
+++ app/src/main/java/com/example/adv160420085week2/GameFragment.kt
@@ -6,12 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_game.*
+import kotlin.random.Random
 
 
 class GameFragment : Fragment() {
 
+    var angka1: Int = 0
+    var angka2: Int = 0
+    var point: Int = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,11 +39,34 @@ class GameFragment : Fragment() {
 //            txtTurn.text = "$playerName's Turn"
 //        }
 
-        val btnBack = view.findViewById<Button>(R.id.btnBack)
-        btnBack.setOnClickListener {
-            val action = GameFragmentDirections.actionMainFragment()
-            Navigation.findNavController(it).navigate(action)
+        val btnAnswer = view.findViewById<Button>(R.id.btnAnswer)
+        val txtNumber = view.findViewById<TextView>(R.id.txtNumber)
+        val txtAnswer = view.findViewById<TextView>(R.id.txtAnswer)
+        RandomAngka()
+
+        btnAnswer.setOnClickListener {
+            CekJawaban()
         }
     }
 
+    private fun RandomAngka() {
+        angka1 = Random.nextInt(101)
+        angka2 = Random.nextInt(101)
+
+        txtNumber.setText("${angka1.toString()} + ${angka2.toString()}")
+    }
+
+    private fun CekJawaban() {
+        val answer = txtAnswer.text.toString()
+        val jawabanUser = answer.toInt()
+        val hasil = angka1 + angka2
+
+        if (jawabanUser == hasil) {
+            RandomAngka()
+            point+=1
+        } else {
+            val action = GameFragmentDirections.actionResultFragment(point)
+            Navigation.findNavController(requireView()).navigate(action)
+        }
+    }
 }
